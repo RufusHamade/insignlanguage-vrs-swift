77551fae107665f2459684d26547cb970c7b7a6e
Developer <developer@Yvonnes-MacBook-Air.local>
2018-02-08 14:59:48 +0000

    UI elements for change password and personal details.

---
+++ InSignLanguage/ChangePasswordController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class ChangePasswordController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
---
+++ InSignLanguage/CustomerDetailsController.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+class CustomerDetailsController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard),
+                                       name: Notification.Name.UIKeyboardWillShow, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard),
+                                       name: Notification.Name.UIKeyboardWillHide, object: nil)
+    }
+
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+
+    @objc func adjustForKeyboard(notification: Notification) {
+        let keyboardRect = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+        let keyboardFrame = self.view.convert(keyboardRect!, from: nil)
+
+        if notification.name == Notification.Name.UIKeyboardWillShow {
+            var contentInset:UIEdgeInsets = self.scrollView.contentInset
+            contentInset.bottom = keyboardFrame.size.height
+            self.scrollView.contentInset = contentInset
+        }
+        else {
+            scrollView.contentInset = UIEdgeInsets.zero
+        }
+    }
+}
